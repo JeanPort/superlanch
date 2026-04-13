b7a78117ff1ad4b694bca62e90c7a7fc82c85eb6
jeanport <pierreanatc522@gmail.com>
2026-04-13 15:10:43 -0300

    Initial commit

---
+++ src/main/java/com/jean/superlanch/SuperlanchApplication.java
@@ -0,0 +1,13 @@
+package com.jean.superlanch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperlanchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperlanchApplication.class, args);
+	}
+
+}
